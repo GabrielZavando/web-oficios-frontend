cd3b57ac9bf62e17ed0a5dd275dddf2e31c6d73a
Esfera Digital <soporte@esferadigital>
2022-05-24 13:23:21 -0400

    Cree las carpetas components, maestros, pages, services y shared. Y los componentes buscador, card, seccion, inicio, usuario, footer y menu dentro de sus correspondientes carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { CardComponent } from './maestros/card/card.component';
+import { SeccionComponent } from './maestros/seccion/seccion.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    InicioComponent,
+    CardComponent,
+    SeccionComponent,
+    BuscadorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/maestros/seccion/seccion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seccion',
+  templateUrl: './seccion.component.html',
+  styleUrls: ['./seccion.component.scss']
+})
+export class SeccionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
