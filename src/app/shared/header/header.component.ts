d605af8cc55dd502a1c70c4d0c2bd5db1032775d
Esfera Digital <soporte@esferadigital>
2022-06-05 22:42:25 -0400

    Añadi la fuente Arvo para el logo, animacion del color de Header y posicionamiento al buscador en la pagina de inicio dentro del slider

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public headerActivo: boolean = false
+  public colorLogo: string = '#fff'
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  headerChangeColor(){
+    let lastScrollTop = 0
+    // al hacer scroll aumenta el valor de sctop
+    let sctop = document.documentElement.scrollTop
+    // console.log(sctop)
+    let st = window.pageXOffset || document.documentElement.scrollTop
+
+
+    if (st > lastScrollTop){
+      return 'Header-activo'
+    }else if (sctop === 0){
+      return 'Header'
+    }
+
+    lastScrollTop = st
+    return 'Header'
+  }
+
+  // Detectar scroll
+  @HostListener('window:scroll', [])
+    onWindowScroll(){
+      this.headerChangeColor()
+    }
 
 }
